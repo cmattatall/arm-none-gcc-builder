02678bf23ee3d04f7f5114e2afc27071d076b195
cmattatall <36604988+cmattatall@users.noreply.github.com>
2021-08-22 02:59:09 -0300

    [Fix] minor details to pass build tests on windows. Windows must build inside a docker file

--- cmake/setup/configure-cmsis.cmake
+++ cmake/setup/configure-cmsis.cmake
@@ -2,7 +2,7 @@ cmake_minimum_required(VERSION 3.20)
 
 FetchContent_Declare(
     cmsis
-    GIT_REPOSITORY "https://github.com/cmattatall/arm-none-gcc-builder.git"
+    GIT_REPOSITORY "https://github.com/cmattatall/CMSIS_5.git"
 )
 
 FetchContent_MakeAvailable(cmsis)
