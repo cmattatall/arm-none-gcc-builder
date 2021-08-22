ec75584717e1a589f6e0ab826feb88ea8bdcfa29
Carl Mattatall <cmattatall2@gmail.com>
2021-08-21 22:58:36 -0300

    [Change] repo url for cmake_git_clone from ssh to https (Repo is now public)

--- cmake/utils/git-utils.cmake
+++ cmake/utils/git-utils.cmake
@@ -5,7 +5,7 @@ if(IS_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/cmake_git_clone)
     message("cmake_git_clone already exists")
 else()
     execute_process(
-        COMMAND git clone git@github.com:cmattatall/cmake_git_clone.git
+        COMMAND git clone "https://github.com/cmattatall/cmake_git_clone.git"
         WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
         RESULTS_VARIABLE GIT_UTILS_RESULT
         OUTPUT_VARIABLE GIT_UTILS_OUTPUT
