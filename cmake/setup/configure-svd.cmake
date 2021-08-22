a19685599901cc1b16b00561ac2cdc71fde3733b
Carl Mattatall <carl.mattatall@rimot.io>
2021-08-21 22:03:50 -0300

    Add stuff

---
+++ cmake/setup/configure-svd.cmake
@@ -0,0 +1,16 @@
+cmake_minimum_required(VERSION 3.20)
+include(ExternalProject)
+include(FetchContent)
+
+
+
+
+
+
+FetchContent_Declare(
+    svd
+    GIT_REPOSITORY git@github.com:cmattatall/cmsis-svd.git
+    GIT_TAG       8ac69ea17f3a8fdc3249f29109531d8b5907596d
+)
+
+FetchContent_MakeAvailable(svd)
