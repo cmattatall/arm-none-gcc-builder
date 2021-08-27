753f7a07f1b6b72454e9e0012cef519b1f1d1f12
Carl Mattatall <cmattatall2@gmail.com>
2021-08-27 12:24:44 -0300

    [Rename] external content targets for stm32cube

--- cmake/fetch-content/fetch-stm32cube.cmake
+++ cmake/fetch-content/fetch-stm32cube.cmake
@@ -7,59 +7,59 @@ cmake_minimum_required(VERSION 3.21)
 
 
 FetchContent_Declare(
-    STM32_CUBE_F0
+    STM32CUBEF0
     GIT_REPOSITORY "https://github.com/STMicroelectronics/STM32CubeF0.git"
 )
-FetchContent_MakeAvailable(STM32_CUBE_F0)
+FetchContent_MakeAvailable(STM32CUBEF0)
 
 FetchContent_Declare(
-    STM32_CUBE_L0
+    STM32CUBEL0
     GIT_REPOSITORY "https://github.com/STMicroelectronics/STM32CubeL0.git"
 )
-FetchContent_MakeAvailable(STM32_CUBE_L0)
+FetchContent_MakeAvailable(STM32CUBEL0)
 
 
 FetchContent_Declare(
-    STM32_CUBE_L1
+    STM32CUBEL1
     GIT_REPOSITORY "https://github.com/STMicroelectronics/STM32CubeL1.git"
 )
-FetchContent_MakeAvailable(STM32_CUBE_L1)
+FetchContent_MakeAvailable(STM32CUBEL1)
 
 
 FetchContent_Declare(
-    STM32_CUBE_F1
+    STM32CUBEF1
     GIT_REPOSITORY "https://github.com/STMicroelectronics/STM32CubeF1.git"
 )
-FetchContent_MakeAvailable(STM32_CUBE_F1)
+FetchContent_MakeAvailable(STM32CUBEF1)
 
 FetchContent_Declare(
-    STM32_CUBE_F2
+    STM32CUBEF2
     GIT_REPOSITORY "https://github.com/STMicroelectronics/STM32CubeF2.git"
 )
-FetchContent_MakeAvailable(STM32_CUBE_F2)
+FetchContent_MakeAvailable(STM32CUBEF2)
 
 FetchContent_Declare(
-    STM32_CUBE_F3
+    STM32CUBEF3
     GIT_REPOSITORY "https://github.com/STMicroelectronics/STM32CubeF3.git"
 )
-FetchContent_MakeAvailable(STM32_CUBE_F3)
+FetchContent_MakeAvailable(STM32CUBEF3)
 
 FetchContent_Declare(
-    STM32_CUBE_F4
+    STM32CUBEF4
     GIT_REPOSITORY "https://github.com/STMicroelectronics/STM32CubeF4.git"
 )
-FetchContent_MakeAvailable(STM32_CUBE_F4)
+FetchContent_MakeAvailable(STM32CUBEF4)
 
 
 FetchContent_Declare(
-    STM32_CUBE_L4
+    STM32CUBEL4
     GIT_REPOSITORY "https://github.com/STMicroelectronics/STM32CubeL4.git"
 )
-FetchContent_MakeAvailable(STM32_CUBE_L4)
+FetchContent_MakeAvailable(STM32CUBEL4)
 
 
 FetchContent_Declare(
-    STM32_CUBE_L5
+    STM32CUBEL5
     GIT_REPOSITORY "https://github.com/STMicroelectronics/STM32CubeL5.git"
 )
-FetchContent_MakeAvailable(STM32_CUBE_L5)
+FetchContent_MakeAvailable(STM32CUBEL5)
