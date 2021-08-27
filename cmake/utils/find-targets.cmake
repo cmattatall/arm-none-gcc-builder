3240a15c348f984bc8a0906e1c8fc3250cd193b0
Carl Mattatall <cmattatall2@gmail.com>
2021-08-27 12:03:28 -0300

    [Add] type to cmake target list util function

--- cmake/utils/find-targets.cmake
+++ cmake/utils/find-targets.cmake
@@ -3,7 +3,14 @@ cmake_minimum_required(VERSION 3.21)
 function(get_all_targets var)
     set(targets)
     get_all_targets_recursive(targets ${CMAKE_CURRENT_SOURCE_DIR})
-    set(${var} ${targets} PARENT_SCOPE)
+
+    set(output_list "") # empty list
+    foreach(target ${targets})
+        get_target_property(type ${target} TYPE)
+        set(current_target_list_entry "${target} (type: ${type})")
+        list(APPEND output_list ${current_target_list_entry})
+    endforeach(target ${targets})
+    set(${var} ${output_list} PARENT_SCOPE)
 endfunction()
 
 macro(get_all_targets_recursive targets dir)
