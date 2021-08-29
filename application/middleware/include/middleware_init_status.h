0c6155090b00dd32909dd9e2e64df5b9fec453a3
Carl Mattatall <cmattatall2@gmail.com>
2021-08-29 08:10:33 -0300

    [Change] project main language to C++

---
+++ application/middleware/include/middleware_init_status.h
@@ -0,0 +1,17 @@
+#ifndef __MIDDLEWARE_INIT_STATUS_H__
+#define __MIDDLEWARE_INIT_STATUS_H__
+#ifdef __cplusplus
+/* clang-format off */
+extern "C"
+{
+/* clang-format on */
+#endif /* Start C linkage */
+
+
+
+#ifdef __cplusplus
+/* clang-format off */
+}
+/* clang-format on */
+#endif /* End C linkage */
+#endif /* __MIDDLEWARE_INIT_STATUS_H__ */
