2319944602e6ea3da4ac7416152eccb551ec8e36
Carl Mattatall <cmattatall2@gmail.com>
2021-08-29 18:26:03 -0300

    [Fix] issue with isr_vector sections and crossbuild linker

--- application/core/src/main.cpp
+++ application/core/src/main.cpp
@@ -3,14 +3,16 @@
 
 #include "middleware.h"
 
+__attribute__((section("isr_vector"))) void foo(void) {}
+
 void application_do_stuff() {
-  struct private_application_struct my_private_application_struct;
-  // do stuff
+    struct private_application_struct my_private_application_struct;
+    // do stuff
 }
 
 int main() {
-  application_do_stuff();
-  while (1) {
-    middlware_talk_to_modem();
-  }
+    application_do_stuff();
+    while (1) {
+        middlware_talk_to_modem();
+    }
 }
