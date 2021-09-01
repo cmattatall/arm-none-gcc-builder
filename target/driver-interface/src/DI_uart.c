e8e066532356143dfcbc632da0cd04e130db7cb4
Carl Mattatall <carl.mattatall@rimot.io>
2021-09-01 15:09:57 -0300

    Separate device select from vendor config targets

--- target/driver-interface/src/DI_uart.c
+++ target/driver-interface/src/DI_uart.c
@@ -1,7 +1,7 @@
 #include "driver_interface.h"
 #include "DI_private_header.h"
 
-#if defined(STM32)
+#if defined(STM32_CHIP)
 #include "stm32f4xx_hal.h"
 #warning USING STM32HAL IMPLEMENTATION FOR DRIVER INTERFACE
 #elif defined(TI_CHIP)
@@ -15,15 +15,8 @@ void DI_private_helper_function() {
 }
 
 int DI_uart_transmit_bytes(uint8_t *buf, uint16_t buflen) {
-#if defined(STM32)
-#if defined(STM32F411xE)
+#if defined(STM32_CHIP)
     // HAL_UART_Transmit( /* ... args ... */);
-#elif defined(STM32F411xE)
-
-#else
-#error NO STM32 DEVICE SPECIFIC IN DI_uart_transmit_bytes
-#endif /* STM DEVICE FAMILY SELECT */
-
 #elif defined(TI_CHIP)
 #warning USING TEXAS INSTRUMENTS DRIVERLIB IMPLEMENTATION FOR DRIVER INTERFACE
 #else
